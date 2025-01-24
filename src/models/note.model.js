6fe1fdba7bc483fd0719c8bba46e13d9feb62eec
Nguyen Minh Nghia <lmhtvui22@gmail.com>
2025-01-24 22:01:57 +0700

    resolve conflict

--- src/models/note.model.js
+++ src/models/note.model.js
@@ -1,11 +1,35 @@
 import db from "../db/db.js";
 
+const masterKey = Buffer.from(process.env.MASTER_KEY, 'hex');
+
 export default class Note {
     constructor() {
         this.schema = process.env.POSTGRES_SCHEMA;
         this.tableName = 'note';
         this.db = db(this.schema);
     }
+
+    static async createTable() {
+        const createTableQuery = `
+            CREATE TABLE IF NOT EXISTS ${this.tableName} (
+                id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+                user_id SERIAL NOT NULL,
+                title VARCHAR(255) NOT NULL,
+                content BYTEA NOT NULL,
+                salt TEXT NOT NULL,
+                iv TEXT NOT NULL,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            );
+        `
+
+        try {
+            await this.db.query(query);
+            console.log(`Table "${this.tableName}" is created or already exists.`);
+        } catch (err) {
+            console.error(`Error creating table "${this.tableName}":`, err);
+        }
+    };
 
     getNoteByID = async (id) => {
         try {
@@ -51,7 +75,7 @@ export default class Note {
 
     deleteNote = async (condition) => {
         try {
-            const res = await this.db.delete(this.tableName, condition);
+            const res = await this.db.delete(this.tableName, null, condition);
             if (!res.success) {
                 return { success: false, message: "Failed to delete" };
             }
