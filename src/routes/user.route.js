a2b97e5e52a59ae3693762b3f301becc422d99f0
Nguyen Minh Nghia <lmhtvui22@gmail.com>
2025-01-24 22:15:27 +0700

    Clear redudance and fix delete error

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -19,10 +19,6 @@ export default class UserRouter {
         this.router.post('/register', this.userController.register);
         this.router.get('/refresh', this.userController.refreshToken);
         this.router.post('/logout', this.userController.logout);
-        this.router.use(authenticate);
-        this.router.get("/profile", (req, res) => {
-            res.render("profile", { user: req.user }); // Trả về trang profile với thông tin user
-        });
     }
 
     getRouter() {
